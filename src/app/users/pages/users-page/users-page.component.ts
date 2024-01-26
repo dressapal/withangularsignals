e250816f1ad7e77b39dfec8d3d6ffc4896bfd155
andressa <andressa.palmieri.s@gmail.com>
2024-01-26 09:47:44 -0300

    first commit

--- src/app/users/pages/users-page/users-page.component.ts
+++ src/app/users/pages/users-page/users-page.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/users';
 
 @Component({
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.css']
 })
-export class UsersPageComponent {
+export class UsersPageComponent implements OnInit{
+
+  public users:User[] = []
+  public currentPage:number = 1;
+
+  constructor(private userService:UsersService){
+
+  }
+
+  ngOnInit(){
+    this.loadPage(this.currentPage);
+  }
+
+  loadPage(page:number){
+    this.userService.loadPage(page).subscribe(
+      users => {
+        console.log(users);
+        this.users = users
+      }
+    )
+  }
 
 }
